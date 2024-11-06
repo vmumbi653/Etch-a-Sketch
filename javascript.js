06a09a8f78b0f371985034575cee59b6faf38e64
vmumbi653 <[mainamumbi23@gmail.com]>
2024-11-06 20:43:20 +0300

    index.html

--- javascript.js
+++ javascript.js
@@ -1,56 +1,35 @@
 const container = document.getElementById('container');
 
-let gridSize = 256;
+const sqr = document.querySelector('.square');
+ let gridSize = 16;
 
 //create square divs
+// function makeDivs(num) {
+//     for(let i=0;i<(num*num);i++) {
+//         let square = document.createElement('div');
+//         square.classList.add('square');
+//         container.appendChild(square);
+//     }
+// }
+// makeDivs();
+// //divs are showing on DOM
+
 function makeDivs(num) {
-    for(let i=0;i<(num*num);i++) {
-        let square = document.createElement('div');
-        square.classList.add('square');
+    const containerSize = 400;
+    container.style.width = `${containerSize}px`;
+    container.style.height = `${containerSize}px`;
+    const squareSize = Math.floor(containerSize / num);
+
+     container.textContent = '';
+
+    for (let i =0; i < num ** 2; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
         container.appendChild(square);
-    }
 }
-makeDivs(16);
-//divs are showing on DOM
-
-// add event listeners for the buttons
-
-const reset = document.querySelector('.resetBtn');
-
-reset.addEventListener('click', function(){
-    let grid = +prompt('Please input a number to set the grid: ');
-
-    if(grid <= 100) {
-        resetBtn(grid);
-    } else {
-        return prompt('Invalid number. Please return a number less than or equal to 100');
-    }
-
-    // resetBtn();
-    // grid.classList.add('square');
-    // container.appendChild(grid);
-
-    // if(grid <= 100) {
-    //     makeDivs(grid);
-    // } else {
-    //     return prompt('Invalid number. Please return a number less than or equal to 100');
-    // }
-});
-
-//do clear button as well//*
-
-function resetBtn(grid) {
-    container.innerHTML = ''; //clear existing grid
-
-    for(let i=0;i<(grid*grid);i++){
-        let grd = document.createElement('div');
-        grd.classList.add('square');
-        container.appendChild(grd);
-
-    }
-}
-
-
 //create hover effect
 const square = document.querySelectorAll('.square');
 
@@ -79,3 +58,77 @@ function randomColor() {
     }
     return 'rgb(' + color.join(',') + ')';
 }
+
+}
+
+//select buttons and add event listeners
+const reset = document.querySelector('.resetBtn');
+reset.addEventListener('click', resetGrid);
+
+function resetGrid() {
+    let num = parseInt(prompt('Please input a number to set the grid: '));
+
+    if(num == NaN || num < 1 || num > 100) {
+        return prompt('Invalid number. Please return a number less than or equal to 100');
+    }else {
+        makeDivs(num);
+    }
+}
+
+// makeDivs(gridSize);
+
+// makeDivs(16);
+
+// // add event listeners for the buttons
+
+// const clearBtn = document.querySelector('.clearBtn');
+
+// clearBtn.addEventListener('click', clearGrid);
+
+// function clearSquare(square) {
+//     square.backgroundColor = 'antiquewhite';
+
+// }
+
+// function clearGrid() {
+//     // let squares = document.querySelectorAll('.square');
+
+//     square.forEach(square => {
+//         clearSquare(square);
+//     });
+// }
+
+
+// const reset = document.querySelector('.resetBtn');
+
+// document.addEventListener('DOMContentLoaded', function() {
+
+
+
+// reset.addEventListener('click', function(){
+//     let grid = +prompt('Please input a number to set the grid: ');
+   
+//     if(grid <= 100) {
+//         resetBtn(grid);
+//     } else {
+//         return prompt('Invalid number. Please return a number less than or equal to 100');
+//     }
+// });
+// })
+
+// function resetBtn(grid) {
+//     container.innerHTML = ''; //clear existing grid
+
+//     for(let i=0;i<(grid*grid);i++){
+//         let grd = document.createElement('div');
+//         grd.classList.add('square');
+//         container.appendChild(grd);
+//     }
+// }
+
+
+
+
+
+
+
